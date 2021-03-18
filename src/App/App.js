2d5f25564bc889666749a3f172c28511e6350ddf
xDeja <61637301+xDeja@users.noreply.github.com>
2021-03-18 12:26:27 -0700

    Update

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,15 @@ function App() {
   return (
     <div>
       Hello World
-      <Button variant="contained" style={{ justifyContent: "center" }}>
+      <Button
+        variant="contained"
+        style={{
+          width: "100%",
+          height: "30%",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
         Just to be sure!
       </Button>
     </div>

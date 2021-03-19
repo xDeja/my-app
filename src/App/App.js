1b14f87b20ae8319c0d6849b37776df459a46ee1
xDeja <61637301+xDeja@users.noreply.github.com>
2021-03-18 17:40:36 -0700

    Changed the colors of the background and the button(s)

--- src/App/App.js
+++ src/App/App.js
@@ -1,41 +1,59 @@
 import "./App.css";
 import React, { useState } from "react";
 import { Button } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import {grey, purple, green, red} from '@material-ui/core/colors/';
 import useWindowSize from '../App/useWindowDimensions'
 
-
-
-
 function App() {
+
   const [width, height] = useWindowSize();
   const [name, setName] = useState("Hello World");
 
   const changeName = () => {
     setName("Hi, Alex!");
   };
+
+  const theme = createMuiTheme({
+    palette: {
+       primary: ({
+         main: grey[900]
+       }),
+       secondary: ({
+         main: red[700]
+       }),
+       error: ({
+         main: red[900]
+       }),
+    },
+  });
+
   return (
     <box>
       <div
         style={{
           position: "absolute",
           bottom: height / 2 - 5,
-          left: width / 2 - 10
+          left: width / 2 - 8,
+          color: "#ffffff",
         }}
       >
         {name}
       </div>
+      <ThemeProvider theme={theme}>
       <Button
         variant="contained"
+        color='primary'
         style={{
           position: "absolute",
-          color: "#000",
-          bottom: height / 1.5,
-          left: width / 2
+          bottom: height/3,
+          left: width/2,
         }}
         onClick={changeName}
       >
-        uh
+        Life is pain
       </Button>
+      </ThemeProvider>
     </box>
   );
 }

5b633889b14631c13a60fda14c4fbd7ec730e039
xDeja <61637301+xDeja@users.noreply.github.com>
2021-03-20 16:33:21 -0700

    Added Konva demo file

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import React, { useState } from "react";
 import { Button } from "@material-ui/core";
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import {grey, purple, green, red} from '@material-ui/core/colors/';
-import useWindowSize from '../App/useWindowDimensions'
+import { Stage, Layer, Rect, Circle } from "react-konva";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { grey, purple, green, red } from "@material-ui/core/colors/";
+import useWindowSize from "../App/useWindowDimensions";
 
 function App() {
-
   const [width, height] = useWindowSize();
   const [name, setName] = useState("Hello World");
 
@@ -16,45 +16,25 @@ function App() {
 
   const theme = createMuiTheme({
     palette: {
-       primary: ({
-         main: grey[900]
-       }),
-       secondary: ({
-         main: red[700]
-       }),
-       error: ({
-         main: red[900]
-       }),
-    },
+      primary: {
+        main: grey[900]
+      },
+      secondary: {
+        main: red[700]
+      },
+      error: {
+        main: red[900]
+      }
+    }
   });
 
   return (
-    <box>
-      <div
-        style={{
-          position: "absolute",
-          bottom: height / 2 - 5,
-          left: width / 2 - 8,
-          color: "#ffffff",
-        }}
-      >
-        {name}
-      </div>
-      <ThemeProvider theme={theme}>
-      <Button
-        variant="contained"
-        color='primary'
-        style={{
-          position: "absolute",
-          bottom: height/3,
-          left: width/2,
-        }}
-        onClick={changeName}
-      >
-        Life is pain
-      </Button>
-      </ThemeProvider>
-    </box>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Rect width={50} height={50} fill="red" />
+        <Circle x={200} y={200} stroke="black" radius={50} />
+      </Layer>
+    </Stage>
   );
 }
 

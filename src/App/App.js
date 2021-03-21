3b70faaaa64975475d962df97ca409925b1eb489
xDeja <61637301+xDeja@users.noreply.github.com>
2021-03-20 17:01:22 -0700

    Added Konva demo file

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,28 @@
 import "./App.css";
 import React, { useState } from "react";
 import { Button } from "@material-ui/core";
-import { Stage, Layer, Rect, Circle } from "react-konva";
+import { Stage, Layer, Star, Text } from 'react-konva';
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { grey, purple, green, red } from "@material-ui/core/colors/";
 import useWindowSize from "../App/useWindowDimensions";
 
+function generateShapes() {
+  return [...Array(10)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    rotation: Math.random() * 180,
+    isDragging: false,
+  }));
+}
+
+const INITIAL_STATE = generateShapes();
+
+
+
 function App() {
   const [width, height] = useWindowSize();
   const [name, setName] = useState("Hello World");
-
-  const changeName = () => {
-    setName("Hi, Alex!");
-  };
 
   const theme = createMuiTheme({
     palette: {
@@ -28,11 +38,58 @@ function App() {
     }
   });
 
+  const [stars, setStars] = React.useState(INITIAL_STATE);
+
+  const handleDragStart = (e) => {
+    const id = e.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id,
+        };
+      })
+    );
+  };
+  const handleDragEnd = (e) => {
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
+
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Stage width={width} height={height}>
       <Layer>
-        <Rect width={50} height={50} fill="red" />
-        <Circle x={200} y={200} stroke="black" radius={50} />
+        <Text text="Try to drag a star" />
+        {stars.map((star) => (
+          <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={star.isDragging ? 10 : 5}
+            shadowOffsetY={star.isDragging ? 10 : 5}
+            scaleX={star.isDragging ? 1.2 : 1}
+            scaleY={star.isDragging ? 1.2 : 1}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          />
+        ))}
       </Layer>
     </Stage>
   );
